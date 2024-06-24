b120820a8a581010dc5983757bd661c3ab73ba58
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-24 19:16:34 +0530

    Initialized routes and DB with prisma and connection pool

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,36 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/api/v1', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('Signup post route')
+});
+
+
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Signin post route')
+});
+
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Blog post route')
+});
+
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Blog put route')
+});
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Blog with id Get route')
+});
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('Bulk blog get route')
+});
+
 
 export default app
