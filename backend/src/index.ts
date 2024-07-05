473f48046ab46b4daba96262c5e032d8382dce0e
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-07-05 11:10:12 +0530

    Signup and signin routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,12 @@ import { Hono } from "hono";
 // Since here nodejs runtime is not being used so the prisma client is imported from the edge
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string;
+    DATABASE_URL: string,
+    JWT_SECRET: string,
   };
 }>();
 
@@ -41,7 +43,7 @@ app.post("/api/v1/user/signup", async (c) => {
       },
     });
 
-    const token: string = "JWT Token Here";
+    const token = await sign({id: newUser.id}, c.env.JWT_SECRET );
 
     console.log(newUser);
     return c.json({
@@ -57,8 +59,41 @@ app.post("/api/v1/user/signup", async (c) => {
 
 
 
-app.post("/api/v1/user/signin", (c) => {
-  return c.text("Signin post route");
+app.post("/api/v1/user/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try{
+    const {email, password } = await c.req.json();
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email
+      }
+    });
+
+    if(!user){
+      c.status(411);
+      return c.json({
+        message: "User not found!"
+      });
+    }
+
+    const token = await sign({id: user.id}, c.env.JWT_SECRET);
+
+    return c.json({
+      message: "Log in successfull!",
+      token: token
+    });
+
+  }catch (e){
+    console.log(`Error while signing in ${e}`);
+    
+    c.status(403);
+    return c.json({
+      message: "Erro while logging!"
+    });
+  }
 });
 
 app.post("/api/v1/blog", (c) => {
