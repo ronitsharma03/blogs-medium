6b4cb53ccdd92a9f080a7ed5e23d1fcd0c0494a4
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-07-07 11:26:13 +0530

    Signup page initialized

---
+++ frontend/src/pages/Signin.tsx
@@ -0,0 +1,14 @@
+import { Quote } from "../components/Quote";
+
+export const Signin = () => {
+  return (
+    <div className="grid grid-cols-2">
+      <div className="">
+
+      </div>
+      <div className="max-lg:hidden">
+        <Quote />
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,14 @@
+import { Quote } from "../components/Quote";
+
+export const Signup = () => {
+  return (
+    <div className="grid grid-cols-2">
+      <div className="">
+        <Quote />
+      </div>
+      <div className="col-span-1">
+
+      </div>
+    </div>
+  );
+};
